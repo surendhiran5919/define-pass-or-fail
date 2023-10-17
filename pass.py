74198720589e16ecfb7027f6b0d20f07ea996d6d
P.SURENDHIRAN <145110568+surendhiran5919@users.noreply.github.com>
2023-10-17 12:42:00 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,10 @@
+a=int(input("tamil :"))
+b=int(input("english :"))
+c=int(input("maths :"))
+d=int(input("science :"))
+e=int(input("social :"))
+sum=a+b+c+d+e
+if (a >40 and b >40 and c >40 and d >40 and e >40):
+     print("Result is Pass")
+else:
+     print("Result is Fail")
